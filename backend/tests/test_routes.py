13cb542a49bd9e35461af8974fdd36d74dd79828
Imtela04 <imtela@gmail.com>
2026-05-20 16:51:16 +0600

    add caching

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
 from main import app
+from unittest.mock import patch, AsyncMock
+from utils.osrm import _cache
 
 client = TestClient(app)
 
@@ -33,3 +35,30 @@ def test_dhaka_to_chittagong():
 
 def test_international():
     assert_valid_route({"lat": 40.7419, "lng": -73.9893}, {"lat": 38.8951, "lng": -77.0364})
+
+
+def test_cache_hits_osrm_once():
+    _cache.clear()  # start fresh
+    with patch("utils.osrm.httpx.AsyncClient") as mock_client:
+        mock_response = AsyncMock()
+        mock_response.json = lambda: {
+            "code": "Ok",
+            "routes": [{
+                "geometry": {
+                    "coordinates": [
+                        [-73.9893, 40.7419],
+                        [-77.0364, 38.8951]
+                    ]
+                }
+            }]
+        }
+        mock_client.return_value.__aenter__.return_value.get = AsyncMock(return_value=mock_response)
+        
+        a = {"lat": 40.7419, "lng": -73.9893}
+        b = {"lat": 38.8951, "lng": -77.0364}
+        
+        client.post("/api/route", json={"a": a, "b": b})
+        print(_cache)
+        client.post("/api/route", json={"a": a, "b": b})
+        
+        assert mock_client.return_value.__aenter__.return_value.get.call_count == 1
--- backend/utils/osrm.py
+++ backend/utils/osrm.py
@@ -1,13 +1,24 @@
 import httpx
 
+_cache = {}
+
 async def fetch_route(a, b):
-    url = f"https://router.project-osrm.org/route/v1/driving/{a.lng},{a.lat};{b.lng},{b.lat}?overview=full&geometries=geojson"
-    try:     
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url, timeout=6)
-            data     = response.json()
-            if data['code'] == 'Ok':
-                return [[lat, lng] for lng, lat in data["routes"][0]["geometry"]["coordinates"]]
-            return [[a.lat, a.lng], [b.lat, b.lng]]
-    except Exception:
-        return [[a.lat, a.lng], [b.lat, b.lng]]
+	key = f"{a.lat},{a.lng},{b.lat},{b.lng}"
+	
+	if key not in _cache:
+		url = f"https://router.project-osrm.org/route/v1/driving/{a.lng},{a.lat};{b.lng},{b.lat}?overview=full&geometries=geojson"
+		try:     
+			async with httpx.AsyncClient() as client:
+				response = await client.get(url, timeout=6)
+				data     = response.json()
+				if data['code'] == 'Ok':
+					result = [[lat, lng] for lng, lat in data["routes"][0]["geometry"]["coordinates"]]
+					_cache[key] = result
+					return _cache[key]
+				else:
+					return [[a.lat, a.lng], [b.lat, b.lng]]
+		except Exception:
+				return [[a.lat, a.lng], [b.lat, b.lng]]
+	return _cache[key]
+	
+			
